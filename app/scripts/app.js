8637675a56c4fde5600a4aa4a54a6133239c7cdd
Julie Bouck <julie@Julies-iMac.local>
2015-03-09 12:05:11 -0700

    svg arrowgit add -Agit add -A

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,34 @@
 // Instantiates global instances / events
 (function($) {
 
+$(document).ready(function() {
+  
+  //set the units for the svg paths
+  $(".path").css("stroke-dashoffset", "1000");
+  $(".path").css("stroke-dasharray", "1000");
+
+  //variable for the 'stroke-dashoffset' unit
+  var $dashOffset = $(".path").css("stroke-dashoffset");//.star
+
+  //on a scroll event - execute function
+  $(window).scroll(function () {    
+
+    //calculate how far down the page the user is 
+    var $percentageComplete = (($(window).scrollTop()/($("html").height()-$(window).height()))*100);    
+
+    //convert dashoffset pixel value to integer
+    var $newUnit = parseInt($dashOffset, 10); 
+
+    //get the value to be subtracted from the 'stroke-dashoffset'
+    var $offsetUnit = $percentageComplete * ($newUnit / 100);
+
+    //set the new value of the dashoffset to create the drawing effect
+    $(".path").css("stroke-dashoffset", $newUnit - $offsetUnit);//.star
+
+  });
+
+});
+
   $('body').bgParallax();
 
   // Section animation waypoints
