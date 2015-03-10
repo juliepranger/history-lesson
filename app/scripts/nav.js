14b00d1c0c90a94127eee52ab9994648c0e8631c
Julie Bouck <julie@Julies-iMac.local>
2015-03-10 11:54:29 -0700

    nav closes after clicking on side link

--- app/scripts/nav.js
+++ app/scripts/nav.js
@@ -3,13 +3,23 @@ navController = function() {
 
 	this.navIcon = document.getElementsByClassName('nav-icon')[0];
 
+	this.navContainer = document.getElementsByClassName('navigation-container')[0];
+
 	this.navIcon.addEventListener('click', function() {
 		ctrl.toggleNav();
 	});
 
+	this.navContainer.addEventListener('click', function(e) {
+		console.log(e.target);
+		ctrl.closeNav();
+	})
+
 };
 
 navController.prototype.toggleNav = function() {
-	this.navContainer = document.getElementsByClassName('navigation-container')[0];
 	this.navContainer.classList.toggle('open');
+};
+
+navController.prototype.closeNav = function() {
+	this.navContainer.classList.remove('open');
 };
